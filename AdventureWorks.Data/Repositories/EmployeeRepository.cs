c66f4386a549eadf988ba46114f97eea6175f16e
cheng93 <derek.c@hotmail.co.uk>
2017-03-07 17:28:34 +0000

    EmployeeRepository

---
+++ AdventureWorks.Data/Repositories/EmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using AdventureWorks.Data.Models;
+using System.Linq;
+
+namespace AdventureWorks.Data.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly IAdventureWorks2014Context _dbContext;
+
+        public EmployeeRepository(IAdventureWorks2014Context dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Employee Get(int id) => _dbContext.Employee.Find(id);
+
+        public IEnumerable<Employee> GetAll() => _dbContext.Employee.ToList();
+    }
+}
---
+++ AdventureWorks.Data/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,9 @@
+﻿using AdventureWorks.Data.Models;
+using AdventureWorks.Data.Repositories.Common;
+
+namespace AdventureWorks.Data.Repositories
+{
+    public interface IEmployeeRepository : IReadOnlyRepository<Employee, int>
+    {
+    }
+}
