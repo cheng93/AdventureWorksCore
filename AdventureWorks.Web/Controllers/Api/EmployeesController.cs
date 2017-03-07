18b84537cb4acee96a5901d2b364bfc24940f682
cheng93 <derek.c@hotmail.co.uk>
2017-03-07 15:22:51 +0000

    Employees api controller

---
+++ AdventureWorks.Web/Controllers/Api/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AdventureWorks.Web.Controllers.Api
+{
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        // GET: api/employees
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/employees/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"value: {id}";
+        }        
+    }
+}
