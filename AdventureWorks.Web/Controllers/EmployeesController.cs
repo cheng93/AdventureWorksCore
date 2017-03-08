fe31fbc7ca8917e4362f1ce82fc11f3809ec1ceb
cheng93 <derek.c@hotmail.co.uk>
2017-03-08 14:18:48 +0000

    EmployeesController and its index layout

---
+++ AdventureWorks.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AdventureWorks.Web.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Employee(int id)
+        {
+            return View();
+        }
+    }
+}
