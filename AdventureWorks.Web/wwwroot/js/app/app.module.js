d313256dfa1456f4d53eaa8e4b4a97a9b11becaa
cheng93 <derek.c@hotmail.co.uk>
2017-03-08 14:17:40 +0000

    app.module.js

---
+++ AdventureWorks.Web/wwwroot/js/app/app.module.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app', [
+        // Angular modules 
+        
+        // Custom modules 
+        'app.employees'
+        // 3rd Party Modules
+
+    ]);
+})();
