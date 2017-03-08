e9214216a3ba491da4761f46f364cc685e4b6e6b
cheng93 <derek.c@hotmail.co.uk>
2017-03-08 14:19:20 +0000

    register employeesController in app.employees

---
+++ AdventureWorks.Web/wwwroot/js/app/employees/employees.controller.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app.employees')
+        .controller('employeesController', employeesController);
+
+    employeesController.$inject = ['$log', 'employeesService'];
+
+    function employeesController($log, employeesService) {
+        /* jshint validthis:true */
+        var vm = this;
+
+        activate();
+
+        function activate() {
+            return employeesService.getEmployees()
+                .then(function (data) {
+                    vm.employees = data;
+                    return vm.employees;
+                });
+        }
+    }
+})();
