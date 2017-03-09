c2581e45e6d065e5b66ec45454c644a0887a523d
cheng93 <derek.c@hotmail.co.uk>
2017-03-09 14:42:09 +0000

    Inject IMapper and use it

--- AdventureWorks.Web/Controllers/Api/EmployeesController.cs
+++ AdventureWorks.Web/Controllers/Api/EmployeesController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using AdventureWorks.Data.Repositories;
+using AutoMapper;
+using AdventureWorks.Web.ViewModels;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,23 +13,27 @@ namespace AdventureWorks.Web.Controllers.Api
     {
         private readonly IEmployeeRepository _employeeRepository;
 
-        public EmployeesController(IEmployeeRepository employeeRepository)
+        private readonly IMapper _mapper;
+
+        public EmployeesController(IEmployeeRepository employeeRepository, IMapper mapper)
         {
             _employeeRepository = employeeRepository;
+            _mapper = mapper;
         }
 
         // GET: api/employees
         [HttpGet]
         public JsonResult Get()
         {
-            return new JsonResult(_employeeRepository.GetAll());
+            return new JsonResult(_mapper.Map<IEnumerable<EmployeeVM>>(_employeeRepository.GetAll()));
         }
 
         // GET api/employees/5
         [HttpGet("{id}")]
         public JsonResult Get(int id)
         {
-            return new JsonResult(_employeeRepository.Get(id));
+            var mapped = _mapper.Map<EmployeeVM>(_employeeRepository.Get(id));
+            return new JsonResult(mapped);
         }        
     }
 }
