9e20fadf278f7e92bf9602960d6ec31b1c4b5dcc
cheng93 <derek.c@hotmail.co.uk>
2017-03-09 14:44:10 +0000

    employeeController

---
+++ AdventureWorks.Web/wwwroot/js/app/employees/employee.controller.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app.employees')
+        .controller('employeeController', employeeController);
+
+    function employeeController() {
+        /* jshint validthis:true */
+        var vm = this;
+
+        activate();
+
+        function activate() { }
+    }
+})();
