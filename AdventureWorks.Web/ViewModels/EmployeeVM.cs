3a92fee8e0060074ef1088d85d425dd675568e6f
cheng93 <derek.c@hotmail.co.uk>
2017-03-09 14:10:46 +0000

    viewmodels

---
+++ AdventureWorks.Web/ViewModels/EmployeeVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AdventureWorks.Web.ViewModels
+{
+    public class EmployeeVM
+    {
+        public int BusinessEntityId { get; set; }
+        public string NationalIdnumber { get; set; }
+        public string LoginId { get; set; }
+        public short? OrganizationLevel { get; set; }
+        public string JobTitle { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Gender { get; set; }
+        public DateTime HireDate { get; set; }
+        public bool SalariedFlag { get; set; }
+        public short VacationHours { get; set; }
+        public short SickLeaveHours { get; set; }
+        public bool CurrentFlag { get; set; }
+
+        public PersonVM Person { get; set; }
+    }
+}
---
+++ AdventureWorks.Web/ViewModels/PersonVM.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventureWorks.Web.ViewModels
+{
+    public class PersonVM
+    {
+        public string PersonType { get; set; }
+        public bool NameStyle { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Suffix { get; set; }
+        public int EmailPromotion { get; set; }
+        public string AdditionalContactInfo { get; set; }
+        public string Demographics { get; set; }
+    }
+}
