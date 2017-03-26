85fda4e19ff85c0f1ec4c88c4edcc0e3d7d69339
cheng93 <derek.c@hotmail.co.uk>
2017-03-26 21:50:26 +0100

    webpack configuration

---
+++ AdventureWorks.Web/config/webpack.common.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+
+var path = './Scripts/js/';
+
+module.exports = {
+  entry: {
+    'polyfills': path + 'polyfills.ts',
+    'vendor': path + 'vendor.ts',
+    'bundle': path + 'main.ts'
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: [
+          {
+            loader: 'ts-loader'
+          },
+          'angular2-template-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['bundle', 'vendor', 'polyfills'],
+      minChunks: Infinity
+    })
+  ]
+};
---
+++ AdventureWorks.Web/config/webpack.dev.js
@@ -0,0 +1,12 @@
+var webpackMerge = require('webpack-merge');
+var commonConfig = require('./webpack.common.js');
+var path = require('path');
+
+module.exports = webpackMerge(commonConfig, {
+    devtool: 'source-map',
+
+    output: {
+        path: path.resolve(__dirname, '../wwwroot/js/'),
+        filename: '[name].js'
+    }
+});
