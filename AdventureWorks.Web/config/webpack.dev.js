3cb7be815173228622dcad83eb139183eff9079f
cheng93 <derek.c@hotmail.co.uk>
2017-03-27 20:22:14 +0100

    ts sourcemaps

--- AdventureWorks.Web/config/webpack.dev.js
+++ AdventureWorks.Web/config/webpack.dev.js
@@ -3,8 +3,7 @@ var commonConfig = require('./webpack.common.js');
 var path = require('path');
 
 module.exports = webpackMerge(commonConfig, {
-    devtool: 'source-map',
-
+    devtool: 'inline-source-map',
     output: {
         path: path.resolve(__dirname, '../wwwroot/js/'),
         filename: '[name].js'
