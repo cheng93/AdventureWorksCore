365ecefd560d0ed4d1cfcb86c8bcb07554add8f6
cheng93 <derek.c@hotmail.co.uk>
2017-03-28 22:54:50 +0100

    Map

--- AdventureWorks.Web/Controllers/Api/EmployeesController.cs
+++ AdventureWorks.Web/Controllers/Api/EmployeesController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using AdventureWorks.Data.Repositories;
 using AutoMapper;
 using AdventureWorks.Web.ViewModels;
+using AdventureWorks.Data.Models;
+using System.Linq;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -25,15 +27,44 @@ namespace AdventureWorks.Web.Controllers.Api
         [HttpGet]
         public JsonResult Get()
         {
-            return new JsonResult(_mapper.Map<IEnumerable<EmployeeVM>>(_employeeRepository.GetAll()));
+            var mapped = _employeeRepository
+                .GetAll()
+                .ToList()
+                .Select(x => Map(x));
+            return new JsonResult(mapped);
         }
 
         // GET api/employees/5
         [HttpGet("{id}")]
         public JsonResult Get(int id)
         {
-            var mapped = _mapper.Map<EmployeeVM>(_employeeRepository.Get(id));
+            var mapped = Map(_employeeRepository.Get(id));
             return new JsonResult(mapped);
-        }        
+        }
+
+        private EmployeeVM Map(Employee employee)
+            => new EmployeeVM()
+            {
+                Id = employee.BusinessEntityId,
+
+                Title = employee.BusinessEntity.Title,
+                FirstName = employee.BusinessEntity.FirstName,
+                MiddleName = employee.BusinessEntity.MiddleName,
+                LastName = employee.BusinessEntity.LastName,
+                Suffix = employee.BusinessEntity.Suffix,
+
+                NationalIdnumber = employee.NationalIdnumber,
+                LoginId = employee.LoginId,
+                OrganizationLevel = employee.OrganizationLevel,
+                JobTitle = employee.JobTitle,
+                BirthDate = employee.BirthDate,
+                MaritalStatus = employee.MaritalStatus,
+                Gender = employee.Gender,
+                HireDate = employee.HireDate,
+                SalariedFlag = employee.SalariedFlag,
+                VacationHours = employee.VacationHours,
+                SickLeaveHours = employee.SickLeaveHours,
+                CurrentFlag = employee.CurrentFlag
+            };
     }
 }
