75ccfecac838387bb04ada0816cd0ca87afee696
cheng93 <derek.c@hotmail.co.uk>
2017-04-06 22:50:53 +0100

    move output into common

--- AdventureWorks.Web/webpack/webpack.dev.js
+++ AdventureWorks.Web/webpack/webpack.dev.js
@@ -1,11 +1,6 @@
 var webpackMerge = require('webpack-merge');
 var commonConfig = require('./webpack.common.js');
-var path = require('path');
 
 module.exports = webpackMerge(commonConfig, {
-    devtool: 'inline-source-map',
-    output: {
-        path: path.resolve(__dirname, '../wwwroot/js/'),
-        filename: '[name].js'
-    }
+    devtool: 'source-map'
 });
