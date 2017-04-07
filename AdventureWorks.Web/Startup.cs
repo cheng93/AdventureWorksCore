14867b10ab7e583b34afe95d91ba66d756452118
cheng93 <derek.c@hotmail.co.uk>
2017-04-08 00:19:34 +0100

    Introduce areas

--- AdventureWorks.Web/Startup.cs
+++ AdventureWorks.Web/Startup.cs
@@ -59,6 +59,10 @@ namespace AdventureWorks.Web
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}");
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
