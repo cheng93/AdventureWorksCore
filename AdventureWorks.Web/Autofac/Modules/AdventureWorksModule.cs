452fa40595a34458d36e9de877f3638ea4d3532b
cheng93 <derek.c@hotmail.co.uk>
2017-04-08 22:03:56 +0100

    Autofac extension and modules

---
+++ AdventureWorks.Web/Autofac/Modules/AdventureWorksModule.cs
@@ -0,0 +1,27 @@
+﻿using AdventureWorks.Data.Models;
+using AdventureWorks.Web.Configurations;
+using Autofac;
+using Microsoft.Extensions.Options;
+using System.Linq;
+using System.Reflection;
+using Module = Autofac.Module;
+
+namespace AdventureWorks.Web.Autofac.Modules
+{
+    public class AdventureWorksModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var references = Assembly.GetEntryAssembly().GetReferencedAssemblies();
+
+            var adventureWorksName = references.Single(x => x.Name == "AdventureWorks.Data");
+            var adventureWorks = Assembly.Load(adventureWorksName);
+
+            builder.RegisterAssemblyTypes(new[] { adventureWorks })
+                .AsImplementedInterfaces();
+
+            builder.Register(x => 
+                new AdventureWorks2014Context(x.Resolve<IOptions<ConnectionStringsOptions>>().Value.AdventureWorks2014));
+        }
+    }
+}
---
+++ AdventureWorks.Web/Autofac/Modules/WideWorldImportersModule.cs
@@ -0,0 +1,28 @@
+﻿using AdventureWorks.Web.Configurations;
+using Autofac;
+using Microsoft.Extensions.Options;
+using System.Linq;
+using System.Reflection;
+using WideWorldImporters.Data.Models;
+using Module = Autofac.Module;
+
+namespace AdventureWorks.Web.Autofac.Modules
+{
+    public class WideWorldImportersModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var references = Assembly.GetEntryAssembly().GetReferencedAssemblies();
+
+            var adventureWorksName = references.Single(x => x.Name == "WideWorldImporters.Data");
+            var adventureWorks = Assembly.Load(adventureWorksName);
+
+            builder.RegisterAssemblyTypes(new[] { adventureWorks })
+                .AsImplementedInterfaces();
+
+            builder.Register(x =>
+                new WideWorldImportersContext(x.Resolve<IOptions<ConnectionStringsOptions>>().Value.WideWorldImporters));
+
+        }
+    }
+}
