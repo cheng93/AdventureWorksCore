b291bb3e6aec37f68b7c3b4a3f65c2d957de06c7
cheng93 <derek.c@hotmail.co.uk>
2017-04-08 20:51:18 +0100

    bootstrap WWI dependencies

--- AdventureWorks.Web/StartupExtensions/StartupInjection.cs
+++ AdventureWorks.Web/StartupExtensions/StartupInjection.cs
@@ -3,6 +3,8 @@ using AdventureWorks.Data.Repositories;
 using AdventureWorks.Web.Configurations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using WideWorldImporters.Data.Models;
+using WideWorldImporters.Data.Repositories;
 
 namespace AdventureWorks.Web.StartupExtensions
 {
@@ -16,6 +18,13 @@ namespace AdventureWorks.Web.StartupExtensions
 
             services.AddScoped<IEmployeeRepository, EmployeeRepository>();
             services.AddScoped<IEmployeeDepartmentHistoryRepository, EmployeeRepository>();
+
+
+            services.AddScoped<WideWorldImportersContext, WideWorldImportersContext>(sp =>
+                new WideWorldImportersContext(sp.GetRequiredService<IOptions<ConnectionStringsOptions>>().Value.WideWorldImporters)
+            );
+
+            services.AddScoped<IOrderRepository, OrderRepository>();
         }
     }
 }
