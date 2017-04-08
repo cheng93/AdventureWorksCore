59a80d344c68471b79b75baf2fa94f9ed60a0506
cheng93 <derek.c@hotmail.co.uk>
2017-04-08 20:50:27 +0100

    OrderRepository

---
+++ WideWorldImporters.Data/Repositories/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using AdventureWorks.Common.Repositories;
+using WideWorldImporters.Data.Models;
+
+namespace WideWorldImporters.Data.Repositories
+{
+    public interface IOrderRepository : IReadOnlyRepository<Orders, int>
+    {
+    }
+}
