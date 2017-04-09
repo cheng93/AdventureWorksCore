bdbbf92d5e9ba5afb139e50914525da81da322ef
cheng93 <derek.c@hotmail.co.uk>
2017-04-09 12:42:37 +0100

    ApiController

---
+++ AdventureWorks.Web/Controllers/ApiController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdventureWorks.Web.Controllers
+{
+    public abstract class ApiController : Controller
+    {
+        protected IMediator Mediator { get; set; }
+
+        protected ApiController(IMediator mediator)
+        {
+            Mediator = mediator;
+        }
+    }
+}
