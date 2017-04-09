1ef51dc173e167505a93095ff7c6182bdab6f517
cheng93 <derek.c@hotmail.co.uk>
2017-04-09 20:40:33 +0100

    GenerationHook for IRequestHandler

---
+++ AdventureWorks.Aop/Hooks/RequestHandlerGenerationHook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using Castle.DynamicProxy;
+
+namespace AdventureWorks.Aop.Hooks
+{
+    public class RequestHandlerGenerationHook : IProxyGenerationHook
+    {
+        public void MethodsInspected()
+        {
+        }
+
+        public void NonProxyableMemberNotification(Type type, MemberInfo memberInfo)
+        {
+        }
+
+        public bool ShouldInterceptMethod(Type type, MethodInfo methodInfo)
+        {
+            return methodInfo.Name == "Handle";
+        }
+    }
+}
