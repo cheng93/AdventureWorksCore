815c492a5f81a92f5daa6dfbc69a8b1da1b864e5
cheng93 <derek.c@hotmail.co.uk>
2017-04-09 20:43:26 +0100

    abstract DataModule which also hooks in AOP

---
+++ AdventureWorks.Web/Autofac/Modules/DataModule.cs
@@ -0,0 +1,36 @@
+﻿using AdventureWorks.Aop.Hooks;
+using AdventureWorks.Aop.Interceptors;
+using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Castle.DynamicProxy;
+using MediatR;
+using System.Reflection;
+using Module = Autofac.Module;
+
+namespace AdventureWorks.Web.Autofac.Modules
+{
+    public abstract class DataModule : Module
+    {
+        private readonly AssemblyName _assemblyName;
+
+        protected DataModule(string assemblyName)
+        {
+            _assemblyName = new AssemblyName(assemblyName);
+        }
+
+        protected abstract void RegisterDataAccess(ContainerBuilder builder);
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            var assembly = Assembly.Load(_assemblyName);
+
+            builder.RegisterAssemblyTypes(assembly)
+                .AsClosedTypesOf(typeof(IRequestHandler<,>))
+                .AsImplementedInterfaces()
+                .EnableClassInterceptors(new ProxyGenerationOptions(new RequestHandlerGenerationHook()))
+                .InterceptedBy(typeof(LoggingInterceptor));
+
+            RegisterDataAccess(builder);
+        }
+    }
+}
