87a6e1e43b8d93c3c6228cc5f0412abb8d9d26c1
cheng93 <derek.c@hotmail.co.uk>
2017-04-09 12:42:02 +0100

    An extension using RequestSettings for EFCore

---
+++ AdventureWorks.Common/Commands/EFCore/EFCoreExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace AdventureWorks.Common.Commands.EFCore
+{
+    public static class EFCoreExtensions
+    {
+        public static IQueryable<T> Apply<T>(this IQueryable<T> query, RequestSettings settings)
+        {
+            return query.Skip(settings.Skip).Take(settings.Take);
+        }
+    }
+}
