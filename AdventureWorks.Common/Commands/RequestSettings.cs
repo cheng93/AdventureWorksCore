6ec8f23ca65594c8c6d45bad51692a20f116aacb
cheng93 <derek.c@hotmail.co.uk>
2017-04-09 12:41:38 +0100

    RequestSettings for IRequest<T>

---
+++ AdventureWorks.Common/Commands/RequestSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventureWorks.Common.Commands
+{
+    public class RequestSettings
+    {
+        public int Take { get; set; }
+
+        public int Skip { get; set; }
+
+        public static RequestSettings Default => new RequestSettings
+        {
+            Take = 1000,
+            Skip = 0
+        };
+    }
+}
