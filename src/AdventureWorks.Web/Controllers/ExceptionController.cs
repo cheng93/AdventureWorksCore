33c285aa2a5680d048e2cb8915e1b90a42406141
cheng93 <derek.c@hotmail.co.uk>
2017-04-14 18:10:11 +0100

    Move exception controller to its own

---
+++ src/AdventureWorks.Web/Controllers/ExceptionController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace AdventureWorks.Web.Controllers
+{
+    public class ExceptionController : Controller
+    {
+        public IActionResult Exception()
+        {
+            throw new Exception("outer exception", new Exception("inner exception"));
+        }
+    }
+}
