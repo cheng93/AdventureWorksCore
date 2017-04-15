bf40f24a33ee58c5234fb10e92ed3672b88bfa40
cheng93 <derek.c@hotmail.co.uk>
2017-04-15 19:45:13 +0100

    remove require

--- src/AdventureWorks.Web/webpack/webpack.dev.js
+++ src/AdventureWorks.Web/webpack/webpack.dev.js
@@ -1,7 +1,6 @@
 var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
 var commonConfig = require('./webpack.common.js');
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = webpackMerge(commonConfig, {
     devtool: 'source-map',
