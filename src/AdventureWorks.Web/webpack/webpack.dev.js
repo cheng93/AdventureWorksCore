23aad771793174c03716baae94ab483febadec6a
cheng93 <derek.c@hotmail.co.uk>
2017-04-15 20:14:02 +0100

    indent

--- src/AdventureWorks.Web/webpack/webpack.dev.js
+++ src/AdventureWorks.Web/webpack/webpack.dev.js
@@ -3,12 +3,12 @@ var webpackMerge = require('webpack-merge');
 var commonConfig = require('./webpack.common.js');
 
 module.exports = webpackMerge(commonConfig, {
-    devtool: 'source-map',
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('development')
-            }
-        })
-    ]
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    })
+  ]
 });
--- src/AdventureWorks.Web/webpack/webpack.prod.js
+++ src/AdventureWorks.Web/webpack/webpack.prod.js
@@ -4,25 +4,25 @@ var commonConfig = require('./webpack.common.js');
 var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = webpackMerge(commonConfig, {
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            beautify: false,
-            mangle: {
-                screw_ie8: true,
-                keep_fnames: true
-            },
-            compress: {
-                screw_ie8: true
-            },
-            comments: false
-        }),
-        new OptimizeCssAssetsPlugin({
-            cssProcessorOptions: { discardComments: { removeAll: true } }
-        })
-    ]
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true
+      },
+      compress: {
+        screw_ie8: true
+      },
+      comments: false
+    }),
+    new OptimizeCssAssetsPlugin({
+      cssProcessorOptions: { discardComments: { removeAll: true } }
+    })
+  ]
 });
