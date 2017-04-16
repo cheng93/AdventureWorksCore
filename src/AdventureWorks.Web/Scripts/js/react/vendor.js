7cfb9c4c3d422c3199a98d51fa2220bbd9051745
cheng93 <derek.c@hotmail.co.uk>
2017-04-16 12:56:59 +0100

    vendor css

--- src/AdventureWorks.Web/Scripts/js/react/vendor.js
+++ src/AdventureWorks.Web/Scripts/js/react/vendor.js
@@ -1,2 +1,12 @@
-﻿import 'react';
+﻿// JS Modules
+import 'react';
 import 'react-dom';
+
+// CSS Modules
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+import 'font-awesome/css/font-awesome.css';
+
+import 'primeng/resources/primeng.css';
+import 'primeng/resources/themes/bootstrap/theme.css';  
