b3636b993be9a4862aaa9632f6b8c020cc06d2d5
cheng93 <derek.c@hotmail.co.uk>
2017-04-16 12:56:46 +0100

    use app.component

--- src/AdventureWorks.Web/Scripts/js/react/main.jsx
+++ src/AdventureWorks.Web/Scripts/js/react/main.jsx
@@ -1,6 +1,8 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {App} from './components/app';
+import {App} from './app/app.component';
+
+import '../../css/site.css';
 
 ReactDOM.render(<App />, document.getElementById('app'));
