95980d964a1f13048b9a71eacf8bacab32546804
cheng93 <derek.c@hotmail.co.uk>
2017-04-17 17:20:12 +0100

    main should be simple, move storelogic into app

--- src/AdventureWorks.Web/Scripts/js/react/app/app.component.jsx
+++ src/AdventureWorks.Web/Scripts/js/react/app/app.component.jsx
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
 
+import {
+  ConnectedRouter,
+  routerReducer,
+  routerMiddleware,
+  push
+} from 'react-router-redux'
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
+import { createBrowserHistory } from 'history';
+
+import { appReducers } from './app.reducers';
 import { AppRouter } from './app.router';
 
-export function App({store}) {
-    return (
-      <Provider store={store}>
+export function App() {
+  let history = createBrowserHistory({
+    basename: '/WideWorldImporters/'
+  });
+
+  let store = createStore(
+    appReducers,
+    applyMiddleware(
+      thunkMiddleware,
+      routerMiddleware(history)
+    ));
+
+  return (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
         <AppRouter />
-      </Provider>
-    );
+      </ConnectedRouter>
+    </Provider>
+  );
 }
--- src/AdventureWorks.Web/Scripts/js/react/main.jsx
+++ src/AdventureWorks.Web/Scripts/js/react/main.jsx
@@ -1,20 +1,10 @@
 ﻿import React from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware  } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-
-import { appReducers } from './app/app.reducers';
 import { App } from './app/app.component';
 
 import '../../css/site.css';
 
-let store = createStore(
-  appReducers,
-  applyMiddleware(
-    thunkMiddleware
-));
-
 render(
-  <App store={store} />,
+  <App />,
   document.getElementById('app')
 );
