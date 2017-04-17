f50b5f4e08e10ef2b0bc37fc3dd3aa2c3e8eba99
cheng93 <derek.c@hotmail.co.uk>
2017-04-17 20:26:02 +0100

    New models

---
+++ src/AdventureWorks.Web/Areas/WideWorldImporters/Models/CustomerVM.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventureWorks.Web.Areas.WideWorldImporters.Models
+{
+    public class CustomerVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/AdventureWorks.Web/Areas/WideWorldImporters/Models/OrderVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AdventureWorks.Web.Areas.WideWorldImporters.Models
+{
+    public class OrderVM
+    {
+        public int Id { get; set; }
+        public CustomerVM Customer { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
